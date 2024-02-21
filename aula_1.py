e127eb5ea5213ae27b5f87ee6bfd980417623fd1
amandaagnerbacchet <bacchetamanda@gmail.com>
2024-02-21 12:10:17 -0300

    Create aula_1.py

---
+++ aula_1.py
@@ -0,0 +1,20 @@
+
+# Obter o nome do cliente a partir da entrada do usuário
+nome = input("Digite o seu nome: ")
+cpf = input("Digite o seu cpf: ")
+endereço = input("Digite o seu endereço: ")
+idade = input("Digite o sua idade: ")
+altura = input("Digite o sua altura: ")
+telefone = input("Digite o seu telefone: ")
+
+
+# Exibir uma mensagem de boas-vindas com o nome do cliente
+print("Olá, " + nome + "! Bem-vindo ao nosso programa.")
+print("seu cpf é" + cpf )
+print("seu endereço é" + endereço )
+print("sua idade é" + idade )
+print("sua altura é" + altura )
+print("sua telefone é" + telefone )
+
+
+
