c3925248739d999959912c17e5d4928ba002a832
amandaagnerbacchet <bacchetamanda@gmail.com>
2024-02-21 12:17:05 -0300

    Create aula_3.py

---
+++ aula_3.py
@@ -0,0 +1,22 @@
+# Aqui fica as strings 
+nome = "python"
+frase = "eu amo programar"
+
+"""
+Uma string em Python é uma sequência de caracteres. 
+Caracteres podem ser letras, números, símbolos, espaços em branco, etc. 
+Em Python, as strings são imutáveis, o que significa que não podem ser alteradas após a sua criação. 
+Você pode criar uma string colocando os caracteres entre aspas simples (') ou aspas duplas (").
+"""
+
+# Aqui fica as listas 
+numeros = [1, 2, 3, 4, 5]
+nomes ["amanda", "julha", "bryan", "gabriel"]
+misturados ["amanda", 2, False, "teste", 1.40]
+
+
+"""
+Uma lista em Python é uma coleção ordenada e mutável de itens. 
+Os itens em uma lista podem ser de qualquer tipo de dado, incluindo outras listas. 
+As listas são definidas usando colchetes ([]) e os elementos são separados por vírgulas.
+"""
